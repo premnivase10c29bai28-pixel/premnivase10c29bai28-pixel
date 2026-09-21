84365777e04b901d44a6780ab8329a238a666326
Prem-nivas-E <premnivas.e10c29_bai28@mepcoeng.ac.in>
2026-09-21 16:33:41 +0530

    Search Insert Position

---
+++ insertpos.c
@@ -0,0 +1,20 @@
+int searchInsert(int* nums, int numsSize, int target) {
+    int l=0,r=numsSize-1;
+    while(l<=r)
+    {
+        int m=l+(r-l)/2;
+        if(nums[m]==target)
+        {
+           return m;
+        }
+        else if(nums[m]<target)
+        {
+            l=m+1;
+        }
+        else
+        {
+            r=m-1;
+        }
+    }
+    return l;
+}
